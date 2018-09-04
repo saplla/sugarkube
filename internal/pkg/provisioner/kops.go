ca9545412bb118aec71563cf80f6804f16cd66cd
Alan Bates <alan.bates@sapient.com>
2018-09-04 11:04:58 +0100

    - Add a function to return whether the kops cluster config exists

--- internal/pkg/provisioner/kops.go
+++ internal/pkg/provisioner/kops.go
@@ -17,6 +17,7 @@
 package provisioner
 
 import (
+	"context"
 	"fmt"
 	"github.com/pkg/errors"
 	"github.com/sugarkube/sugarkube/internal/pkg/clustersot"
@@ -26,6 +27,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 type KopsProvisioner struct {
@@ -34,6 +36,44 @@ type KopsProvisioner struct {
 
 // todo - make configurable
 const KOPS_PATH = "kops"
+
+// Returns whether a kops cluster config has already been created (this doesn't check whether the cluster is actually
+// running though).
+func (p KopsProvisioner) clusterConfigExists(sc *kapp.StackConfig, providerImpl provider.Provider) (bool, error) {
+
+	providerVars := provider.GetVars(providerImpl)
+	log.Debugf("Checking if Kops cluster config exists for values: %#v", providerVars)
+
+	provisionerValues := providerVars[PROVISIONER_KEY].(map[interface{}]interface{})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel() // The cancel should be deferred so resources are cleaned up
+
+	args := []string{
+		"get",
+		"clusters",
+		"--state",
+		provisionerValues["state"].(string),
+	}
+
+	cmd := exec.CommandContext(ctx, KOPS_PATH, args...)
+	cmd.Env = os.Environ()
+
+	err := cmd.Run()
+	if ctx.Err() == context.DeadlineExceeded {
+		return false, errors.New("Timed out trying to retrieve kops cluster config")
+	}
+	if err != nil {
+		if _, ok := err.(*exec.ExitError); ok {
+			log.Debug("Cluster config doesn'te exist")
+			return false, nil
+		} else {
+			return false, errors.Wrap(err, "Error fetching kops clusters")
+		}
+	}
+
+	return true, nil
+}
 
 func (p KopsProvisioner) create(sc *kapp.StackConfig, providerImpl provider.Provider,
 	dryRun bool) error {
@@ -92,10 +132,28 @@ func (p KopsProvisioner) ClusterSot() (clustersot.ClusterSot, error) {
 }
 
 func (p KopsProvisioner) isAlreadyOnline(sc *kapp.StackConfig, providerImpl provider.Provider) (bool, error) {
+	configExists, err := p.clusterConfigExists(sc, providerImpl)
+	if err != nil {
+		return false, errors.WithStack(err)
+	}
+
+	if !configExists {
+		return false, nil
+	}
+
 	panic("not implemented")
 }
 
 // No-op function, required to fully implement the Provisioner interface
 func (p KopsProvisioner) update(sc *kapp.StackConfig, providerImpl provider.Provider) error {
+	configExists, err := p.clusterConfigExists(sc, providerImpl)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	if !configExists {
+		return nil
+	}
+
 	panic("not implemented")
 }
